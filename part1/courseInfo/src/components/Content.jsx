c64aa5bb9fa20c66d66c70d43d755a8338884a1a
bruxo <diogo.stein@outlook.com.br>
2023-08-07 10:14:34 -0300

    Remove console.log from courseInfo

--- part1/courseInfo/src/components/Content.jsx
+++ part1/courseInfo/src/components/Content.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import Part from './Part';
 
 const Content = ({ parts }) => {
-  console.log(parts);
   return (
     <div>
-      {' '}
       <Part name={parts[0].name} exercises={parts[0].exercises} />
       <Part name={parts[1].name} exercises={parts[1].exercises} />
       <Part name={parts[2].name} exercises={parts[2].exercises} />
--- part1/courseInfo/src/components/Total.jsx
+++ part1/courseInfo/src/components/Total.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Total = ({ parts }) => {
   return (
     <p>
-      Number of exercises{' '}
+      Number of exercises {''}
       {parts[0].exercises + parts[1].exercises + parts[2].exercises}
     </p>
   );
